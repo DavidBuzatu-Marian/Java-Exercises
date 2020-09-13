ebd3d89eafd9f6f07f249743cf0a524026a510bf
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-09-13 13:34:27 +0100

    September commit

---
+++ DesignPatterns - Command Pattern/src/GarageDoor.java
@@ -0,0 +1,22 @@
+public class GarageDoor {
+
+    public void up() {
+        System.out.println("The door is Open!");
+    }
+
+    public void down() {
+        System.out.println("The door is Closed!");
+    }
+
+    public void stop() {
+        System.out.println("The door is stopped!");
+    }
+
+    public void lightOn() {
+        System.out.println("Light is on!");
+    }
+
+    public void lightOff() {
+        System.out.println("Light is off!");
+    }
+}
---
+++ DesignPatterns - Command Pattern/src/SimpleRemoteControl.java
@@ -0,0 +1,12 @@
+public class SimpleRemoteControl {
+
+    private Command slot;
+
+    public void setCommand(Command command) {
+        slot = command;
+    }
+
+    public void buttonWasPressed() {
+        slot.execute();
+    }
+}
